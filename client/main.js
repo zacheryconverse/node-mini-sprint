b1b0a155302e948cd552e5d11e2f5708add7f6a8
Zachery Converse <zacheryconverse@gmail.com>
2020-05-29 19:40:31 -0600

    Learn a lot of server and node

--- client/main.js
+++ client/main.js
@@ -10,15 +10,24 @@ $(document).ready(function() {
     addQuote(quote);
   });
 
+
   function getQuote(){
-
     //YOUR CODE HERE, Add a GET request
-
+    $.get(`http://localhost:3000/quote`, function( data ) {
+      // let quote = $('#quote');
+      // quote.append(`<h1>${data}</h1>`)
+      console.log(data)
+      $("#response").append(data);
+    });
   }
 
   function addQuote(quote){
-    
-    //YOUR CODE HERE, Add a POST request
 
+    //YOUR CODE HERE, Add a POST request
+    $.post(`http://localhost:3000/quote`, quote, response => {
+      $('form')[0].reset(); // clear the input field
+      // $('#response').text(`Thank you for your submission: ${response}`);
+      $("#response").append(quote);
+    });
   }
 });
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+// server ***
+
 const http = require('http');
 
 //headers to allows CORS requests
-const headers = {
+const headers = { // problly wanna use this
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
@@ -12,11 +14,11 @@ const port = 3000;
 
 // TODO: Fill with strings of your favorite quotes :)
 const quotes = [
-  'one',
-  'two',
-  'three',
-  'four',
-  'five'
+  'Be yourself; everyone else is already taken.',
+  'So many books, so little time.',
+  "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+  'You only live once, but if you do it right, once is enough.',
+  "If you tell the truth, you don't have to remember anything."
 ];
 
 //Utility Function to return a random integer
@@ -26,6 +28,10 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
+function getRandomQuote(arr, cb) {
+  return quotes[getRandomInt(0, quotes.length - 1)];
+}
+
 const handleRequest = function(req, res) {
   console.log(`Endpoint: ${req.url} Method: ${req.method}`);
 
@@ -33,17 +39,29 @@ const handleRequest = function(req, res) {
   if (req.url == '/') {
     console.log('redirecting');
     res.writeHead(301, {...headers, Location: `http://localhost:${port}/quote`}) //redirect to quote
-    res.end();
+    res.end('what???');
   }
 
   // TODO: GET ONE
-  if ((req.url == '/quote/' || req.url == '/quote') && req.method == "FILL ME IN") {
-    //YOUR CODE HERE
-
+  if ((req.url == '/quote/' || req.url == '/quote') && req.method == "GET") {
+    //YOUR CODE HERE - node docs - headers, res.writehead, res.write, res.end - not returning with keyword return, built-in response methods
+    // console.log('here') // send back quote from random and quote functions
+    res.writeHead(200, headers);
+    res.write(getRandomQuote(quotes[getRandomInt(0, quotes.length - 1)]));
+    res.end();
   }
   // TODO: POST/CREATE
-  else if ((req.url == 'FILL ME IN' || req.url == 'FILL ME IN') && req.method == "FILL ME IN") {
+  else if ((req.url == '/quote/' || req.url == '/quote') && req.method == "POST") {
     //YOUR CODE HERE
+    console.log('oh')
+    let body = '';
+    req.on('data', function(chunk) {
+      body += chunk;
+    });
+    req.on('end', function() {
+      res.writeHead(200, headers);
+      res.end(body);
+    });
   }
 
 //CATCH ALL ROUTE
